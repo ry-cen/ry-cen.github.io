dbc45c974949141d32a77c799400506456a027f3
Ryan Cen <rycen618@gmail.com>
2022-10-22 00:41:57 -0700

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
@@ -15,6 +14,9 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
 	declarations: [
@@ -24,18 +26,21 @@ import { FooterComponent } from './footer/footer.component';
 		AboutComponent,
 		ContactComponent,
 		HeaderComponent,
-  		FooterComponent
+  		FooterComponent,
+    EducationComponent,
+    SkillsComponent
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		MatButtonModule,
 		MatCardModule,
-		MatSidenavModule,
 		MatIconModule,
 		BrowserAnimationsModule,
-		RouterModule
+		RouterModule,
+		NgxSpinnerModule,
 	],
+	exports: [NgxSpinnerModule],
 	providers: [],
 	bootstrap: [AppComponent],
 	schemas: [CUSTOM_ELEMENTS_SCHEMA]
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,7 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  projects = [{"name": "Portfolio Site"}, {"name": "Multiplayer Shogi Game"}, {"name": "Cash Wallet"}, {"name": "L-Store Database"}, {"name": "3D Model Viewer"}, {"name" : "Rush Hour Solver"}]
+  projects = [
+    { "name": "Portfolio Site", 
+      "link": "https://github.com/ry-cen/portfolio-site", 
+      "tools": "JavaScript / Angular",
+      "desc": "Fully responsive website that displays projects, skills, and information about me.", 
+      "bullets": ["Utilized Angular to create a responsive navigation bar that becomes a side menu on mobile.", 
+                "Designed and implemented an easily scalable projects section using components."]}, 
+    { "name": "Multiplayer Shogi Game", 
+      "link": "https://github.com/ry-cen/shogi-game",
+      "tools": "JavaScript / React / Node.js",
+      "desc": "Full stack web app that allows two players to play Shogi against each other.", 
+      "bullets": ["Implemented model-view-controller design pattern to communicate between UI and game model.", 
+                  "Designed fully interactive React front-end to handle piece movement and game logic.",
+                  "Created a socket based back-end to manage game instances and communicate moves between clients."]}, 
+    { "name": "Cash Wallet", 
+      "link": "https://github.com/ry-cen/cash-wallet",
+      "tools": "C++ / Stripe API",
+      "desc": "Back-end for a peer to peer payment system with thread concurrency for transactions.", 
+      "bullets": ["Implemented thread safe processes for modifying user information, depositing money, and transferring money.",
+                  "Utilized Stripe’s API to process payments through credit card when depositing money into an account."]}, 
+    { "name": "L-Store Database", 
+      "link": "https://github.com/ry-cen/lstore-database",
+      "tools": "Python",
+      "desc": "Web app that can display OBJ files and allows the user to move the camera, models and light sources.", 
+      "bullets": ["Planned implementation of the database system and designed overall system architecture.", 
+                  "Lead the group and divided work for parts of the project among group members to meet project deadlines.",
+                  "Optimized run time of reading and writing records by 80% and 90% respectively during refactor."]}, 
+    { "name": "3D Model Viewer", 
+      "link": "https://github.com/ry-cen/3d-model-viewer",
+      "tools": "JavaScript / WebGL / GLSL",
+      "desc": "Web app that can display OBJ files and allows the user to move the camera, models and light sources.", 
+      "bullets": ["Wrote code in GLSL for Phong and Gouraud shading in the scene with point and directional light sources.",
+                  "Calculated model view projection matrix for perspective and orthographic projection based on scene properties.",
+                  "Developed object based scene and implemented raycasting to select models and light sources to modify."]}, 
+    { "name" : "Rush Hour Solver", 
+      "link": "https://github.com/ry-cen/RushHourSolver",
+      "tools": "JavaScript / React / Node.js / Python",
+      "desc": "Program that uses A* search algorithm to find the optimal solution to a given Rush Hour puzzle.", 
+      "bullets": ["Utilizes backtracking and A* to return every move used in order to get to the optimal solution.",
+                  "Interactive front-end to configure the puzzle to solve before running the script and displaying the solution."]}
+  ]
 
   constructor() { }
 
